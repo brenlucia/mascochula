070289cfdb07dfbf7fbe831b6aa241a1d82a569a
Brenda Gomez <brendalucia410@gmail.com>
2023-07-26 01:03:03 -0300

    agregando modificaciones

--- js/main.js
+++ js/main.js
@@ -37,44 +37,49 @@ document.addEventListener("DOMContentLoaded", function () {
     }
   
     // Evento para capturar la consulta y aumentar el contador
-    document.querySelector(".botonEnviar").addEventListener("click", function () {
-      const nameInput = document.getElementById("floatingInput").value.trim();
-      const emailInput = document.getElementById("floatingEmail").value.trim();
-      const messageInput = document.getElementById("floatingTextarea").value.trim();
+document.querySelector(".botonEnviar").addEventListener("click", function () {
+    const nameInput = document.getElementById("floatingInput").value.trim();
+    const emailInput = document.getElementById("floatingEmail").value.trim();
+    const messageInput = document.getElementById("floatingTextarea").value.trim();
   
-      // Validar que se hayan ingresado datos antes de contar la consulta
-      if (nameInput !== "" && emailInput !== "" && messageInput !== "") {
-        cantidadConsultasRegistradas++;
-        actualizarCantidadConsultas();
+    // Validar que se hayan ingresado datos antes de contar la consulta
+    if (nameInput !== "" && emailInput !== "" && messageInput !== "") {
+      cantidadConsultasRegistradas++;
+      actualizarCantidadConsultas();
   
-        // Limpiar el formulario después de contar la consulta
-        document.getElementById("floatingInput").value = "";
-        document.getElementById("floatingEmail").value = "";
-        document.getElementById("floatingTextarea").value = "";
+      // Limpiar el formulario después de contar la consulta
+      document.getElementById("floatingInput").value = "";
+      document.getElementById("floatingEmail").value = "";
+      document.getElementById("floatingTextarea").value = "";
   
-        // Mostrar un mensaje de agradecimiento
-        console.log("Gracias por dejar tu consulta.");
-      } else {
-        // Mostrar un mensaje de error si no se han ingresado todos los datos requeridos
-        console.log("Por favor, completa todos los campos antes de enviar la consulta.");
-      }
-    });
+      // Mostrar un mensaje de agradecimiento
+      alert("Gracias por dejar tu consulta.");
+    } else {
+      // Mostrar un mensaje de error si no se han ingresado todos los datos requeridos
+      console.log("Por favor, completa todos los campos antes de enviar la consulta.");
+    }
+  });
   
-    // Ejemplo de uso de las funciones para operaciones matemáticas
-    const resultadoSuma = sumar(5, 3);
-    const resultadoResta = restar(10, 4);
-    const resultadoMultiplicacion = multiplicar(6, 2);
-    const resultadoDivision = dividir(12, 3);
   
-    console.log(`Resultado suma: ${resultadoSuma}`);
-    console.log(`Resultado resta: ${resultadoResta}`);
-    console.log(`Resultado multiplicación: ${resultadoMultiplicacion}`);
-    console.log(`Resultado división: ${resultadoDivision}`);
+    // Prompt para realizar operaciones matemáticas
+    const num1 = parseFloat(prompt("¿Cuántos gatos tenes?:"));
+    const num2 = parseFloat(prompt("¿Cuántos perros tenes?:"));
+    const num3 = parseFloat(prompt("¿Tenes conejos? ¿Cuántos?:"));
   
-    // Ejemplo de uso de la función para calcular porcentaje
-    const objetivo = 100;
-    const porcentaje = calcularPorcentaje(cantidadConsultasRegistradas, objetivo);
+    if (!isNaN(num1) && !isNaN(num2)) {
+      // Ejemplo de uso de las funciones para operaciones matemáticas
+      const resultadoSuma = sumar(num1, num2);
+      
   
-    console.log(`Porcentaje de consultas registradas: ${porcentaje}%`);
+      console.log(`Resultado suma: ${resultadoSuma}`);
+      
+      // Ejemplo de uso de la función para calcular porcentaje
+      const objetivo = 100;
+      const porcentaje = calcularPorcentaje(cantidadConsultasRegistradas, objetivo);
+  
+      console.log(`Porcentaje de consultas registradas: ${porcentaje}%`);
+    } else {
+      console.log("Por favor, ingresa números válidos para realizar las operaciones.");
+    }
   });
   
