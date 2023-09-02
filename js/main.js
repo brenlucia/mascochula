d34d8f4e686037854cd1e9a79c800c98cd943ac1
Brenda Gomez <brendalucia410@gmail.com>
2023-09-02 17:59:35 -0300

    agregando ultimos detalles

--- js/main.js
+++ js/main.js
@@ -5,6 +5,7 @@ const mensajeTextarea = document.getElementById('floatingTextarea');
 const botonEnviar = document.querySelector('.botonEnviar');
 const botonReset = document.querySelector('.botonReset');
 const cantidadConsultasSpan = document.getElementById('cantidadConsultas');
+const listaPacientes = document.getElementById('listaPacientes'); // Agregamos un elemento para mostrar la lista de pacientes
 
 // Agregar manejadores de eventos
 botonEnviar.addEventListener('click', enviarFormulario);
@@ -48,11 +49,39 @@ function resetearFormulario() {
     mensajeTextarea.value = '';
 }
 
-// Mostrar consultas almacenadas al cargar la página
-function mostrarConsultasAlmacenadas() {
-    let consultas = JSON.parse(localStorage.getItem('consultas')) || [];
-    cantidadConsultasSpan.textContent = `Cantidad de consultas registradas: ${consultas.length}`;
+// Función para obtener y mostrar datos ficticios de pacientes desde una API
+async function obtenerPacientes() {
+    try {
+        const response = await fetch('https://jsonplaceholder.typicode.com/users');
+        if (!response.ok) {
+            throw new Error('No se pudo obtener la lista de pacientes');
+        }
+        const pacientes = await response.json();
+        mostrarPacientes(pacientes);
+    } catch (error) {
+        console.error('Error al obtener pacientes:', error);
+    }
 }
 
-// Llamamos a la función al cargar la página
-mostrarConsultasAlmacenadas();
+// Función para mostrar pacientes en el HTML
+function mostrarPacientes(pacientes) {
+    listaPacientes.innerHTML = ''; // Limpiamos cualquier contenido anterior
+
+    // Aquí agregamos pacientes ficticios a la lista de pacientes
+    const paciente1 = document.createElement('li');
+    paciente1.textContent = 'Nombre: Juan Perez, Email: juan@example.com';
+
+    const paciente2 = document.createElement('li');
+    paciente2.textContent = 'Nombre: María García, Email: maria@example.com';
+
+    // Agregamos los pacientes ficticios a la lista
+    listaPacientes.appendChild(paciente1);
+    listaPacientes.appendChild(paciente2);
+
+    // Luego, agregamos los pacientes reales desde la API
+    pacientes.forEach((paciente) => {
+        const li = document.createElement('li');
+        li.textContent = `Nombre: ${paciente.name}, Email: ${paciente.email}`;
+        listaPacientes.appendChild(li);
+    });
+}
